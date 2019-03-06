eccbf96647c54916256e6c147b4f4fb7798610e0
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 12:33:02 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,19 @@ def first_challenge
   }
 
   #your code here
-
+contact.each do |person,data|
+  if person == "Freddy Mercury"
+  data.each do |attribute,value|
+ if attribute == :favorite_icecream_flavors
+ value.each do |flavour|
+   if flavour == "strawberry"
+     value.delete(flavour)
+   end
+ end 
+ end 
+ end 
+  end 
+end
 contacts
   #remember to return your newly altered contacts hash!
   contacts
