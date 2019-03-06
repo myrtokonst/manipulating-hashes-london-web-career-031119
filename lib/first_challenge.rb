bcde6aa523042092eb842a08ad3374d831501b71
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 12:33:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contact.each do |person,data|
+contacts.each do |person,data|
   if person == "Freddy Mercury"
   data.each do |attribute,value|
  if attribute == :favorite_icecream_flavors
