7f215cda57f18f43ef456926afff549e9b6eaa82
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 12:16:19 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ contacts.each do |person, data|
   if data == "Freddy Mercury"
     data.each do |things, values|
       if values == favorite_icecream_flavors
-        favorite_icecream_flavors.delete("strawberry")
+        favorite_icecream_flavors.unshift()
       end 
       end
     end 
